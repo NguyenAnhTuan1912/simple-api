568420a9aa4266be7d009638082761a27649d8e8
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-04-20 13:48:32 +0700

    Develop: Init some component

--- client/src/layouts/ThreeColumnLayout.tsx
+++ client/src/layouts/ThreeColumnLayout.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export default function ThreeColumnLayout() {
+type ThreeColumnLayoutProps = {
+  leftSide: () => JSX.Element | JSX.Element;
+  rightSide: () => JSX.Element | JSX.Element;
+  mainSide: () => JSX.Element | JSX.Element;
+}
+
+export default function ThreeColumnLayout(props: ThreeColumnLayoutProps) {
   return (
-    <div>ThreeColumnLayout</div>
+    <div>
+      {/* Left side */}
+      {
+        typeof props.leftSide === "function"
+          ? <div>{props.leftSide()}</div>
+          : <div>{props.leftSide}</div>
+      }
+      {/* Main side */}
+      {
+        typeof props.mainSide === "function"
+          ? <div>{props.mainSide()}</div>
+          : <div>{props.mainSide}</div>
+      }
+      {/* Right side */}
+      {
+        typeof props.rightSide === "function"
+          ? <div>{props.rightSide()}</div>
+          : <div>{props.rightSide}</div>
+      }
+    </div>
   )
 }
